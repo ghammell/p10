24d1551ff6e3c286a350cbe9de346b1013f3e86d
NokoGary 3 JF <ghammell1@gmail.com>
2014-09-09 16:50:33 -0700

    added homepage

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,10 @@
-https://foursquare.com/oauth2/authenticate
-    ?client_id=TWGEEI2BJU45QBLFVXFHVLMJVPXDGBAOC4B0VSYCWJDYFGRQ
-    &response_type=code
-    &redirect_uri=YOUR_REGISTERED_REDIRECT_URI
+get '/'  do
+  erb :home_page
+end
+
+
+
+# https://foursquare.com/oauth2/authenticate
+#     ?client_id=TWGEEI2BJU45QBLFVXFHVLMJVPXDGBAOC4B0VSYCWJDYFGRQ
+#     &response_type=code
+#     &redirect_uri=YOUR_REGISTERED_REDIRECT_URI
