21eedda12af02c56e63912a9b531966c7ffbcef8
NokoGary 3 JF <ghammell1@gmail.com>
2014-09-09 17:17:13 -0700

    add signed-in page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,21 @@
 get '/'  do
+  # if params[:code]
+  #   session[:user_id] = params[:code]
+  # end
+  p params[:code]
   erb :home_page
 end
 
 get '/sign-in' do
   client_id = "TWGEEI2BJU45QBLFVXFHVLMJVPXDGBAOC4B0VSYCWJDYFGRQ"
   redirect_url = "http://fathomless-fortress-1403.herokuapp.com/"
+
   redirect "https://foursquare.com/oauth2/authenticate?client_id=" + client_id + "&response_type=code&redirect_uri=" + redirect_url
+end
+
+get '/signed-in' do
+  @code = params[:code]
+  erb :signed_in
 end
 
 
